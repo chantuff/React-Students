f55da4e70b7823ce186ec53307e89023bb444b9a
chantuff <chan.tuff@yahoo.com>
2023-11-13 10:38:09 -0500

    removed header

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -4,7 +4,6 @@ const Student = ({ name, bio, scores }) => (
   <div>
     <h2>{name}</h2>
     <p>{bio}</p>
-    <h3>Scores:</h3>
     {scores.map((score, index) => (
       <div key={index}>
         <p>Date: {score.date}</p>
